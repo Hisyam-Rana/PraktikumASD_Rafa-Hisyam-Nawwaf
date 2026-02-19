06794b8607bece7e6e0058ad75cc863b3014dc30
Hisyam-Rana <rhn287@gmail.com>
2026-02-19 10:30:56 +0700

    Practicum 1

---
+++ Jobsheet_2_19022026/practicum_1_Mahasiswa_19.java
@@ -0,0 +1,37 @@
+package Jobsheet_2_19022026;
+
+public class practicum_1_Mahasiswa_19 {
+
+    String name;
+    String nim;
+    String clasS;
+    double gpa;
+
+    void displayInformation () {
+        System.out.println("Name    : "+name);
+        System.out.println("NIM     : "+nim);
+        System.out.println("GPA     : "+gpa);
+        System.out.println("Class   : "+clasS+"\n");
+    }
+
+    void changeclass (String newClass) {
+        clasS = newClass;
+    }
+
+    void updateGPA (double newGPA) {
+        gpa = newGPA;
+    }
+
+    String performanceValue () {
+        if (gpa >= 3.5) {
+            return "Excellent performance";
+        } else if (gpa >= 3.0) {
+            return "Good performance";
+        } else if (gpa >= 2.0) {
+            return "Adequate performance";
+        } else {
+            return "Poor perfonmance";
+        }
+    } 
+}
+
