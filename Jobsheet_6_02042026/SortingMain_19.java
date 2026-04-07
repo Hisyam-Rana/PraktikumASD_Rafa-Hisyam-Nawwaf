5f40477ef7ba29f14480ea2f0e8e6238704ed268
Hisyam-Rana <rhn287@gmail.com>
2026-04-07 21:17:28 +0700

    Bubble sort

---
+++ Jobsheet_6_02042026/SortingMain_19.java
@@ -0,0 +1,14 @@
+package Jobsheet_6_02042026;
+
+public class SortingMain_19 {
+    public static void main(String[] args) {
+        int[] a = {34, 7, 23, 32, 5, 62};
+
+        Sorting_19 sorting = new Sorting_19(a);
+        System.out.println("Original Array : ");
+        sorting.print();
+        sorting.bubbleSort();
+        System.out.println("Sorted array (Bubble sort) : ");
+        sorting.print();
+    }
+}
---
+++ Jobsheet_6_02042026/Sorting_19.java
@@ -0,0 +1,34 @@
+package Jobsheet_6_02042026;
+
+public class Sorting_19 {
+    int[] data;
+    int size;
+
+    public Sorting_19 (int[] data) {
+        this.data = data;
+        this.size = data.length;
+    }
+
+    public void bubbleSort () {
+        for (int i = 0; i < size -1; i++) {
+            for (int j = 0; j < size -i -1; j++) {
+                if (data [j] > data [j+1]) {
+                    int temp = data[j];
+                    data[j] = data [j+1];
+                    data [j+1] = temp; 
+                }
+            }
+        }
+    }
+
+    public void print () {
+        for (int num : data) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
+
+
+
+
