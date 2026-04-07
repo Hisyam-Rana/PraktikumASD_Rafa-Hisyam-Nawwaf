fe37dca6c60bcb3b70ae61b2e72ad9e272401dce
Hisyam-Rana <rhn287@gmail.com>
2026-04-07 21:39:22 +0700

    Insertion sort

--- Jobsheet_6_02042026/SortingMain_19.java
+++ Jobsheet_6_02042026/SortingMain_19.java
@@ -3,13 +3,15 @@ package Jobsheet_6_02042026;
 public class SortingMain_19 {
     public static void main(String[] args) {
         //int[] a = {34, 7, 23, 32, 5, 62};
-        int[] b = {30, 20, 2, 8, 14};
+        //int[] b = {30, 20, 2, 8, 14};
+        int[] c = {40, 10, 4, 9, 3};
         //Sorting_19 sorting = new Sorting_19(a);
-        Sorting_19 sorting2 = new Sorting_19(b);
+        //Sorting_19 sorting2 = new Sorting_19(b);
+        Sorting_19 sorting3 = new Sorting_19(c);
         System.out.println("Original Array : ");
-        sorting2.print();
-        sorting2.selectionSort();
+        sorting3.print();
+        sorting3.insertionSort();
         System.out.println("Sorted array (Selection sort) : ");
-        sorting2.print();
+        sorting3.print();
     }
 }
