9aca690f0a6461aace19c21fdb92f0a21e5af86f
Hisyam-Rana <rhn287@gmail.com>
2026-04-07 21:28:13 +0700

    Selection sort

--- Jobsheet_6_02042026/SortingMain_19.java
+++ Jobsheet_6_02042026/SortingMain_19.java
@@ -2,12 +2,14 @@ package Jobsheet_6_02042026;
 
 public class SortingMain_19 {
     public static void main(String[] args) {
-        int[] a = {34, 7, 23, 32, 5, 62};
-        Sorting_19 sorting = new Sorting_19(a);
+        //int[] a = {34, 7, 23, 32, 5, 62};
+        int[] b = {30, 20, 2, 8, 14};
+        //Sorting_19 sorting = new Sorting_19(a);
+        Sorting_19 sorting2 = new Sorting_19(b);
         System.out.println("Original Array : ");
-        sorting.print();
-        sorting.bubbleSort();
-        System.out.println("Sorted array (Bubble sort) : ");
-        sorting.print();
+        sorting2.print();
+        sorting2.selectionSort();
+        System.out.println("Sorted array (Selection sort) : ");
+        sorting2.print();
     }
 }
--- Jobsheet_6_02042026/Sorting_19.java
+++ Jobsheet_6_02042026/Sorting_19.java
@@ -18,6 +18,19 @@ public class Sorting_19 {
             }
         }
     }
+    public void selectionSort () {
+        for (int i = 0; i < size -1; i++) {
+            int minIndex = i;
+            for (int j = i+1; j < size; j++) {
+                if (data [j] < data [minIndex]) {
+                    minIndex = j;
+                }
+                int temp = data [minIndex];
+                data [minIndex] = data [i];
+                data [i] = temp;
+            }
+        }
+    }
     public void print () {
         for (int num : data) {
             System.out.print(num + " ");
