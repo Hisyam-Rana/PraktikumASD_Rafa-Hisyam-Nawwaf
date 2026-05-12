689c20a917329eb5909045880eaefae93a3ee1b7
Hisyam-Rana <rhn287@gmail.com>
2026-05-12 21:45:26 +0800

    Experiment 02

--- Jobsheet_12_07052026/SLLMain.java
+++ Jobsheet_12_07052026/SLLMain.java
@@ -5,17 +5,17 @@ public class SLLMain {
         Scanner isc = new Scanner(System.in);
         SingleLinkedList sll = new SingleLinkedList();
 
-        System.out.print("NIM   : "); 
-        String nim = isc.nextLine();
-        System.out.print("Nama  : "); 
-        String name = isc.nextLine();
-        System.out.print("Class : "); 
-        String cls = isc.nextLine();
-        System.out.print("GPA   : "); 
-        double gpa = isc.nextDouble();
+        // System.out.print("NIM   : "); 
+        // String nim = isc.nextLine();
+        // System.out.print("Nama  : "); 
+        // String name = isc.nextLine();
+        // System.out.print("Class : "); 
+        // String cls = isc.nextLine();
+        // System.out.print("GPA   : "); 
+        // double gpa = isc.nextDouble();
 
-        Student19 stdNew = new Student19(nim, name, cls, gpa);
-        sll.addLast(stdNew);
+        // Student19 stdNew = new Student19(nim, name, cls, gpa);
+        // sll.addLast(stdNew);
 
         Student19 std1 = new Student19("001", "Student 01", "TI-1I", 3.89);
         Student19 std2 = new Student19("002", "Student 02", "TI-1I", 3.45);
@@ -28,8 +28,22 @@ public class SLLMain {
         sll.addLast(std1);
         sll.print();
         sll.insertAfter(std3, "Student 04");
-        sll.insertArt(2, std2);
+        sll.insertAt(2, std2);
+    
+        System.out.println("Data at index 1 is : ");
+        Student19 data = sll.getData(1);
+        data.print();
+
+        int idx = sll.indexOf("Student 01");
+        System.out.println("Student 01 is located at index : "+idx);
+
+        sll.removeFirst();
+        sll.removeLast();
         sll.print();
+        sll.removeAt(0);
+        sll.print();
+        
+        
         isc.close();
     }
 }
--- Jobsheet_12_07052026/SingleLinkedList.java
+++ Jobsheet_12_07052026/SingleLinkedList.java
@@ -59,7 +59,7 @@ public class SingleLinkedList {
         } while (temp != null);
     }
     
-    public void insertArt(int index, Student19 std) {
+    public void insertAt(int index, Student19 std) {
         if (index < 0) {
             System.out.println("Wrong index!!");
         } else if (index == 0) {
@@ -72,6 +72,97 @@ public class SingleLinkedList {
             temp.next = new Node(std, temp.next);
             if (temp.next.next == null) {
                 tail = temp.next;
+            }
+        }
+    }
+
+    Student19 getData(int idx) {
+        if (isEmpty()) {
+            System.out.println("LinkedList is empty!!");
+            return null;
+        }
+        Node tmp = head;
+        for (int i = 0; i < idx; i++) {
+            tmp = tmp.next;
+        }
+        return tmp.data;
+    }
+
+    int indexOf(String key) {
+        if (isEmpty()) {
+            System.out.println("LinkedList is empty!!");
+            return -1;
+        }
+        Node tmp = head;
+        int idx = 0;
+        while (tmp != null && !tmp.data.name.equalsIgnoreCase(key)) {
+            tmp = tmp.next;
+            idx++;
+        }
+        if (tmp == null) {
+            return -1;
+        } else {
+            return idx;
+        }
+    }
+
+    void removeFirst() {
+        if (isEmpty()) {
+            System.out.println("LinkedList is empty!!");
+        } else if (head==tail) {
+            head = tail = null;
+        } else {
+            head = head.next;
+        }    
+    }
+
+    void removeLast() {
+        if (isEmpty()) {
+            System.out.println("LinkedList is empty!!");
+        } else if (head==tail) {
+            head = tail = null;
+        } else {
+            Node tmp = head;
+            while (tmp.next != tail) {
+                tmp = tmp.next;
+            }
+            tmp.next = null;
+            tail = tmp;
+        }    
+    }
+
+    public void remove(String key) {
+        if (isEmpty()) {
+            System.out.println("LinkedList is empty!!");
+        } else {
+            Node temp = head;
+            while (temp != null) {
+                if ((temp.data.name.equalsIgnoreCase(key)) && (temp == head)) {
+                    removeFirst();
+                    break;
+                } else if (temp.next.data.name.equalsIgnoreCase(key)) {
+                    temp.next = temp.next.next;
+                    if (temp.next == null) {
+                        tail = temp;
+                    }
+                    break;
+                }
+                temp = temp.next;
+            }
+        }
+    }
+
+    public void removeAt(int index) {
+        if (index == 0) {
+            removeFirst();
+        } else {
+            Node temp = head;
+            for (int i = 0; i < index -1; i++) {
+                temp = temp.next;
+            }
+            temp.next = temp.next.next;
+            if (temp.next == null) {
+                tail =temp;
             }
         }
     }
