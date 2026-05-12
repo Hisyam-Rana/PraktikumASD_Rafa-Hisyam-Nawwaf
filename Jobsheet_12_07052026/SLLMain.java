fe0f78cdacf83a981f44e87eb76d5488be53eae4
Hisyam-Rana <rhn287@gmail.com>
2026-05-12 20:40:28 +0800

    Modified for question Number 3

--- Jobsheet_12_07052026/SLLMain.java
+++ Jobsheet_12_07052026/SLLMain.java
@@ -1,8 +1,21 @@
 package Jobsheet_12_07052026;
-
+import java.util.Scanner;
 public class SLLMain {
     public static void main(String[] args) {
+        Scanner isc = new Scanner(System.in);
         SingleLinkedList sll = new SingleLinkedList();
+
+        System.out.print("NIM   : "); 
+        String nim = isc.nextLine();
+        System.out.print("Nama  : "); 
+        String name = isc.nextLine();
+        System.out.print("Class : "); 
+        String cls = isc.nextLine();
+        System.out.print("GPA   : "); 
+        double gpa = isc.nextDouble();
+
+        Student19 stdNew = new Student19(nim, name, cls, gpa);
+        sll.addLast(stdNew);
 
         Student19 std1 = new Student19("001", "Student 01", "TI-1I", 3.89);
         Student19 std2 = new Student19("002", "Student 02", "TI-1I", 3.45);
@@ -17,5 +30,6 @@ public class SLLMain {
         sll.insertAfter(std3, "Student 04");
         sll.insertArt(2, std2);
         sll.print();
+        isc.close();
     }
 }
